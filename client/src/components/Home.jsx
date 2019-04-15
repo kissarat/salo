ce5deacae7c18100c4626d4e5ebb73c439f577e7
Taras Labiak <tlabiak@ukeess.com>
2019-04-15 15:57:25 +0300

    + setRef in Home component

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
 
-function Home() {
-  return (
-    <div className="home">Environment: {process.env.NODE_ENV} </div>
-  );
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: false
+    }
+  }
+
+  setRef(element) {
+    console.log(element);
+    this.element = element;
+  }
+
+  render() {
+    return (
+      <div className="home">
+        <p>Environment: {process.env.NODE_ENV}</p>
+        <button type="button" onClick={() => this.setState({show: !this.state.show})}>{this.state.show ? 'Hide' : 'Show'}</button>
+        {this.state.show ? <div ref={e => this.setRef(e)}>Element</div> : null}
+      </div>
+    );
+  }
 }
 
 export default Home;
