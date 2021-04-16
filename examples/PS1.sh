c18849028c766a8310ed1e88af59a055f6f9adf3
Taras Labiak <kissarat@gmail.com>
2021-04-16 21:59:05 +0300

    add more helpfull scripts

---
+++ examples/PS1.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# PS1 examples
+
+PS1="\W \[$(tput sgr0)\]\[\033[38;5;2m\]\@\[$(tput sgr0)\]\[\033[38;5;15m\]\\$ \[$(tput sgr0)\]"
---
+++ examples/profile.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+export PATH=$HOME/Public/github.com/kissarat/salo/bin:/usr/local/Cellar/openjdk\@11/11.0.10/bin:/usr/local/Cellar/openjdk/15.0.2/bin:$PATH
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
