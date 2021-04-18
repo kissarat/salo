34921aedc7b185368789c64a9e55dcce98f5476c
Taras Labiak <kissarat@gmail.com>
2021-04-18 17:21:58 +0300

    + install macOS

--- examples/macos/darwin.sh
+++ examples/macos/darwin.sh
@@ -4,6 +4,8 @@ nvm install lts
 curl https://install.meteor.com/ | sh
 brew install nvm
 ~/.profile <<EOF
+export PS1="\e[0;35m\$(date +%k:%M:%S)\e[m \e[0;32m\w\e[m$ "
+
 export NVM_DIR="$HOME/.nvm"
 [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
 [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
